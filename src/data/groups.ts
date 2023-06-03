110e004f4595737258f06c4038f9b2d8012166f4
Alejandro LR <alejandro.lr.developer@hotmail.com>
2023-06-03 11:34:40 -0500

    Add: Group type definition and dummy groups

---
+++ src/data/groups.ts
@@ -0,0 +1,14 @@
+import type IGroup from "../types/group";
+
+const groups: Array<IGroup> = [
+  {
+    id: "70ebf64b-80b1-48f0-aaa5-03454202d0a6",
+    name: "Latam",
+  },
+  {
+    id: "b1c4dfaf-5ef8-467f-86fd-530dba311ce5",
+    name: "Eroupe 2023",
+  },
+];
+
+export default groups;
