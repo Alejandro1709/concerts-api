6f3407f160bc4285014dba87123ff5978eeec0f5
Alejandro LR <alejandro.lr.developer@hotmail.com>
2023-06-07 18:54:14 -0500

    Add: conceert controller

--- src/controllers/concert.controller.ts
+++ src/controllers/concert.controller.ts
@@ -1,5 +1,58 @@
+import asyncHandler from "express-async-handler";
+import {
+  Concert,
+  type ConcertType,
+  type ConcertWithId,
+  Concerts,
+} from "../models/concert.model";
+import slugify from "slugify";
 import type { Request, Response } from "express";
 
-export const getConcerts = (req: Request, res: Response) => {
-  res.status(200).json({ message: "Hello" });
-};
+export const getConcerts = asyncHandler(
+  async (_req: Request, res: Response<ConcertWithId[]>) => {
+    const result = await Concerts.find();
+
+    const concerts = await result.toArray();
+
+    res.status(200).json(concerts);
+  },
+);
+
+export const getConcert = asyncHandler(
+  async (req: Request, res: Response<ConcertWithId>) => {
+    const { slug } = req.params;
+
+    const concert = await Concerts.findOne({ slug });
+
+    if (!concert) {
+      throw new Error("This Concert does not exists!");
+    }
+
+    res.status(200).json(concert);
+  },
+);
+
+export const createConcert = asyncHandler(
+  async (
+    req: Request<Record<string, never>, ConcertWithId, ConcertType>,
+    res: Response<ConcertWithId>,
+  ) => {
+    const result = await Concert.parseAsync(req.body);
+
+    const newer = {
+      ...result,
+      slug: slugify(result.title, { lower: true }),
+    };
+
+    const savedConcert = await Concerts.insertOne(newer);
+
+    if (!savedConcert.acknowledged) {
+      throw new Error("Error while inserting document...");
+    }
+
+    res.status(201).json({
+      _id: savedConcert.insertedId,
+      ...result,
+    });
+  },
+);
--- src/routes/concert.routes.ts
+++ src/routes/concert.routes.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
 import { validateRequest } from "../middlewares/validate.middleware";
-import { getConcerts } from "../controllers/concert.controller";
+import {
+  getConcerts,
+  getConcert,
+  createConcert,
+} from "../controllers/concert.controller";
+import { Concert, type ConcertType } from "../models/concert.model";
 
 const router = Router();
 
-router.route("/").get(getConcerts);
+router
+  .route("/")
+  .get<Record<string, never>, ConcertType[]>(getConcerts)
+  .post(validateRequest({ body: Concert }), createConcert);
+
+router.route("/:slug").get<Record<string, any>, ConcertType>(getConcert);
 
 export default router;
