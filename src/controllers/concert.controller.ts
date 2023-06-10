10d0be49272e1e1ae131fdac52ea71260d07e1e8
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-06-09 20:40:07 -0500

    Replace: Old mongoDB queries with mongoose

--- src/controllers/concert.controller.ts
+++ src/controllers/concert.controller.ts
@@ -1,27 +1,23 @@
 import asyncHandler from "express-async-handler";
-import { type ConcertWithId, Concerts } from "../models/concert.model";
+import Concert from "../models/Concert";
 import type { Request, Response } from "express";
 
 export const getConcerts = asyncHandler(
-  async (_req: Request, res: Response<ConcertWithId[]>) => {
-    const result = await Concerts.find();
-
-    const concerts = await result.toArray();
+  async (_req: Request, res: Response) => {
+    const concerts = await Concert.find();
 
     res.status(200).json(concerts);
   },
 );
 
-export const getConcert = asyncHandler(
-  async (req: Request, res: Response<ConcertWithId>) => {
-    const { slug } = req.params;
+export const getConcert = asyncHandler(async (req: Request, res: Response) => {
+  const { slug } = req.params;
 
-    const concert = await Concerts.findOne({ slug });
+  const concert = await Concert.findOne({ slug });
 
-    if (!concert) {
-      throw new Error("This Concert does not exists!");
-    }
+  if (!concert) {
+    throw new Error("This Concert does not exists!");
+  }
 
-    res.status(200).json(concert);
-  },
-);
+  res.status(200).json(concert);
+});
