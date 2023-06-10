e63479afe5db7f5263d03c02c0918c20dadc66b7
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-06-09 19:41:05 -0500

    Add: mongoose connection func

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,5 @@
 import { MongoClient } from "mongodb";
+import mongoose, { MongooseError } from "mongoose";
 import { ENV, MONGO_URI_DEV, MONGO_URI_PROD } from "./secrets";
 
 const MONGO_URI = (
@@ -11,4 +12,19 @@ export const db = client.db();
 export const connect = async () => {
   await client.connect();
   console.log("Connected to MongoDB!");
+};
+
+// mongoose
+export const connectDb = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+
+    console.log("MongoDB connected");
+  } catch (err) {
+    if (err instanceof MongooseError) {
+      console.log(err.message);
+    }
+    console.log(err);
+    process.exit(1);
+  }
 };
